fb6db0b05b83e59dcbc3c16b0782ffb791e10e9a
navyad <navyad.pro@gmail.com>
2022-05-27 18:13:56 +0530

    stage #1

---
+++ app/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+}
