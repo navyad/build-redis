a5b99f6d543fff46857629c4d133da76c59a42d5
navyad <navyad.pro@gmail.com>
2022-05-30 14:42:02 +0530

    stage2: pong

--- app/server.go
+++ app/server.go
@@ -7,16 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	tcp_listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port: 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	fmt.Println("Accepting connection")
+
+	conn, err := tcp_listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write([]byte("+PONG\r\n"))
 }
