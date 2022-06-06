b7483445f329fc21d4c3975d8d58ee7c6887474e
navyad <navyad.pro@gmail.com>
2022-06-06 16:11:47 +0530

    stage3: send multiple pongs

--- app/server.go
+++ app/server.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+
+
+func handelConnection(conn net.Conn) {
+	for {
+		buf := make([]byte, 124)
+		arrayLen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		data := string(buf[:arrayLen])
+		if strings.Contains(data, "ping") {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+        }
+}
+
 
 func main() {
 	tcp_listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -15,9 +33,12 @@ func main() {
 	fmt.Println("Accepting connection")
 
 	conn, err := tcp_listener.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	} else {
+		conn.Write([]byte("+PONG\r\n"))
 	}
-	conn.Write([]byte("+PONG\r\n"))
+	handelConnection(conn)
 }
